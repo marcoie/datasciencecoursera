f076341040af5402c3bbc9206dfbe4483df87686
marco illescas <marco.illescas@gmail.com>
2017-08-02 21:52:31 -0400

    Additional Comments Added for the Steps in Code

--- GettingAndCleaningData_Final/run_analysis.R
+++ GettingAndCleaningData_Final/run_analysis.R
@@ -22,30 +22,29 @@ features$feature_name <- gsub(',', '-', features$feature_name, fixed = TRUE) ##r
     features$feature_name <- gsub('angle(Z-gravityMean)','angleOfZ-gravityMean', features$feature_name, fixed = TRUE)
 
 
-##Read the Data for train set
+##Read the Data for train set - set proper column names and combine Subject, Activity and Sensor Data in 1 DF
 x_train <- read.table("./UCI HAR Dataset/train/X_train.txt", col.names = features$feature_name)
 y_train <- read.table("./UCI HAR Dataset/train/Y_train.txt", col.names = c("activity_id")) %>% 
     left_join(activity_labels,by = "activity_id") %>% 
     select(activity_name) ##get JUST activity labels
 s_train <- read.table("./UCI HAR Dataset/train/subject_train.txt", col.names = c("subject_id"))
-
-#combine Subject, Activity, and Sensor Data
 full_train <- cbind(s_train, y_train, x_train)
 
+##Read the Data for test set - set proper column names and combine Subject, Activity and Sensor Data in 1 DF
 x_test <- read.table("./UCI HAR Dataset/test/X_test.txt", col.names = features$feature_name)
 y_test <- read.table("./UCI HAR Dataset/test/Y_test.txt", col.names = c("activity_id")) %>% 
     left_join(activity_labels,by = "activity_id") %>% 
     select(activity_name) ##get activity and labels
 s_test <- read.table("./UCI HAR Dataset/test/subject_test.txt", col.names = c("subject_id"))
-
-#combine Subject, Activity, and Sensor Data
 full_test <- cbind(s_test, y_test, x_test)
 
+#Assembly ONE single experiment dataset
 experiment_data <- rbind(full_train, full_test)
 
 #Extracts only the measurements of mean and std for each experiment line
 selected_set <- as_tibble(cbind(experiment_data[,1:2], experiment_data[,grepl("mean",names(experiment_data))], experiment_data[,grepl("std",names(experiment_data))]))
 
+##Create New TIDY set with "means" computed and grouped by Subject and activity and write to a new TEXT File
 new_tidy_tbl <- selected_set %>% group_by(subject_id, activity_name) %>% summarise_all(funs(mean, "mean",mean(.,na.rm=TRUE)))
-##write.csv(new_tbl, file = "new_tidy_dataset.csv")
 write.table(new_tidy_tbl, file = "new_tidy_dataset.txt", row.names = FALSE)
+##write.csv(new_tbl, file = "new_tidy_dataset.csv")
